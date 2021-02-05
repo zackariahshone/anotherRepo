a2de3f894859e24fc0357327bb00219229ab811f
Zack Shone <zackariahshone.ta@gmail.com>
2021-02-05 12:24:24 -0700

    m

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path')
 const router = express.Router();
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, './public/html/index.html'));
 });
 
-app.use(express.static('index.html'))
+app.use(express.static('public'))
 
 app.listen(process.env.PORT || 8000, ()=>console.log('listening on 8000'))
